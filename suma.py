140711fe5aa30d464ce898e394308c547a301181
asloaisiga <asloaisiga@uamv.edu.ni>
2025-10-22 18:09:10 -0600

    Agrego programa Python Suma -Alexa

---
+++ suma.py
@@ -0,0 +1,6 @@
+print("Bienvenido al programa de suma de dos números.")
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+suma = num1 + num2
+print("------------------------------------------")
+print("La suma de", num1, "y", num2, "es:", suma)
